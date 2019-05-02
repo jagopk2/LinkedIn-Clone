48d333c6710cd43afcf844af452e3a910fbe31ea
Muhammad junaid <jagopk2@gmail.com>
2019-05-02 19:58:57 +0500

    =imagework

--- React/src/Components/userRegister.js
+++ React/src/Components/userRegister.js
@@ -13,40 +13,65 @@ class UserRegister extends Component {
       $('select').formSelect();
     });
   }
+  state = {
+    image: null
+  }
+
+  imageChange = (event) => {
+    console.log(event.target.files[0])
+    this.setState({
+      image: event.target.files[0]
+    })
+  }
 
   handleSubmit = (e) => {
-    var firstname = e.target.first_name.value 
+    var firstname = e.target.first_name.value
     var lastname = e.target.last_name.value
     var phonenumber = e.target.phoneNumber.value
-    var address =  e.target.address.value
+    var address = e.target.address.value
     var field = e.target.field.value
     var email = e.target.email.value
-    var password =e.target.password.value
+    var password = e.target.password.value
     e.preventDefault()
-    console.log(firstname)
-    console.log(lastname)
-    console.log(phonenumber)
-    console.log(address)
-    console.log(field)
-    console.log(email)
-    console.log(password)
-    console.log('handling submit')
+    // console.log(firstname)
+    // console.log(lastname)
+    // console.log(phonenumber)
+    // console.log(address)
+    // console.log(field)
+    // console.log(email)
+    // console.log(password)
+    // console.log('handling submit')
 
-    let url = 'http://localhost:3002/users/signup'
-    axios.post(url, {
-      firstname,
-      lastname,
-      phonenumber,
-      address,
-      field,
-      email,
-      password,
-    })
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
+
+
+    /// Image Upload Logic
+    var image = this.state.image;
+    const data = new FormData();
+    data.append("image", image, image.name);
+    // console.log(image.name)
+    // console.log(data)
+    return axios.post(`http://localhost:3002/` + 'upload', data)
+      .then(response => {
+        // console.log(response.data.imageUrl);
+        let url = 'http://localhost:3002/users/signup'
+        axios.post(url, {
+          firstname,
+          lastname,
+          phonenumber,
+          address,
+          field,
+          email,
+          password,
+          picture : response.data.imageUrl
+        })
+          .then(function (response) {
+            console.log(response.data);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }).catch((err) => {
+        console.log(err);
       });
 
   }
@@ -98,6 +123,7 @@ class UserRegister extends Component {
               </select>
               <label>Field of Intrest</label>
             </div>
+            <input type="file" name="" id="" onChange={this.imageChange} />
             <button className="btn waves-effect waves-light" type="submit" name="action">
               Submit
                         <i className="material-icons right">send</i>
